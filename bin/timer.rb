49fce98dd76fd29db925910639581dfef5db323f
Derek Carter <dlcarter@users.noreply.github.com>
2024-04-15 09:00:01 -0700

    Multi-platform

--- bin/timer.rb
+++ bin/timer.rb
@@ -21,9 +21,49 @@ def pad(str)
   str.to_s.rjust(2, "0")
 end
 
-# def go(num_minutes)
+ENVIRONMENTS = {
+  linux: 0,
+  mac: 1,
+  windows: 2
+}
+
+def os
+  @_os ||= if `which osascript` && $?.success?
+    ENVIRONMENTS[:mac]
+  elsif `which spd-say` && $?.success?
+    ENVIRONMENTS[:linux]
+  elsif `which espeak` && $?.success?
+    ENVIRONMENTS[:windows]
+  end
+end
+
+def mac?
+  os == ENVIRONMENTS[:mac]
+end
+
+def clear_terminal
+  if mac?
+    `osascript -e 'if application "Terminal" is frontmost then tell application "System Events" to keystroke "k" using command down'`
+  else
+    `clear -x`
+  end
+end
+
+def alert(message="")
+  threads = []
+  if `which say` && $?.success?
+    # threads << Thread.new { `say "Your #{num_minutes} minute #{title} timer is up."` }
+    # threads << Thread.new { `osascript -e 'display alert "#{TIMES_UP_ALERT_TITLE}" message "#{TIMES_UP_ALERT_BODY}" as critical'` }
+    threads << Thread.new { `say "#{message}"` }
+    threads << Thread.new { `osascript -e 'display alert "#{message}'` }
+  else
+    threads << Thread.new { "spd-say #{message}" }
+  end
+  threads.each(&:join)
+end
+
 def go(num_minutes, title="")
-  `osascript -e 'if application "Terminal" is frontmost then tell application "System Events" to keystroke "k" using command down'`
+  clear_terminal
   puts "Number of minutes: #{num_minutes}"
   puts "Title: #{title}"
 
@@ -50,10 +90,8 @@ def go(num_minutes, title="")
     sleep 1
   end
 
-  threads = []
-  threads << Thread.new { `say "Your #{num_minutes} minute #{title} timer is up."` }
-  threads << Thread.new { `osascript -e 'display alert "#{TIMES_UP_ALERT_TITLE}" message "#{TIMES_UP_ALERT_BODY}" as critical'` }
-  threads.each(&:join)
+  alert "Your #{num_minutes} minute #{title} timer is up."
+
 end
 
 go ARGV[0], ARGV[1]
