edf6bbb7c8facf741f68117b4317bd635b8b1016
Derek Carter <dlcarter@users.noreply.github.com>
2025-08-27 11:21:31 -0700

    Add mac bluetooth fix

---
+++ bluetooth.sh
@@ -0,0 +1,6 @@
+; Turn on
+sudo defaults write /Library/Preferences/com.apple.airport.bt.plist bluetoothCoexMgmt Hybrid
+
+; Turn off
+sudo defaults delete /Library/Preferences/com.apple.airport.bt.plist bluetoothCoexMgmt
+
